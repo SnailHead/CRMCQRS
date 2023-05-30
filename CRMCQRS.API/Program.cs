dae82ea7aeb2536bcca1b397a94d38619e191b7a
Snail <07862493175a>
2023-05-30 12:48:45 +0400

    add UseHsts

--- CRMCQRS.API/Program.cs
+++ CRMCQRS.API/Program.cs
@@ -23,7 +23,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
-
+app.UseHsts();
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
